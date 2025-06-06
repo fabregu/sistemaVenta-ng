f2098491e617288de18780eb29b0d882354d9e60
Wilfredo Abregu <fabregu01@gmail.com>
2025-06-06 14:30:48 -0500

    feat: Implentacion Historial Ventas y funcionalidades

--- src/app/Components/layout/Pages/historial-venta/historial-venta.component.ts
+++ src/app/Components/layout/Pages/historial-venta/historial-venta.component.ts
@@ -1,15 +1,125 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit, ViewChild, AfterViewInit } from '@angular/core';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatDialog } from '@angular/material/dialog';
+
+import * as moment from 'moment';
+
+import { ModalDetalleVentaComponent } from '../../Modals/modal-detalle-venta/modal-detalle-venta.component';
+
+import { VentaService } from 'src/app/Services/venta.service';
+import { UtilidadService } from 'src/app/Reutilizables/utilidad.service';
+import { Venta } from 'src/app/Interfaces/venta';
+
+export const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMMM YYYY'
+  },
+};
 
 @Component({
   selector: 'app-historial-venta',
   templateUrl: './historial-venta.component.html',
-  styleUrls: ['./historial-venta.component.css']
+  styleUrls: ['./historial-venta.component.css'],
+  providers: [
+    { provide: MY_DATE_FORMATS, useValue: MY_DATE_FORMATS }
+  ]
 })
-export class HistorialVentaComponent implements OnInit {
 
-  constructor() { }
+export class HistorialVentaComponent implements OnInit, AfterViewInit {
 
-  ngOnInit(): void {
+  frmBusqueda: FormGroup;
+  opcionesBusqueda: any[] = [
+    { value: 'fecha', descripcion: 'Por Fechas' },
+    { value: 'numero', descripcion: 'Numero Venta' }
+  ];
+
+  columnasTablas: string[] = ['fechaRegistro', 'numeroDocumento', 'tipoPago', 'total', 'accion'];
+  datoInicio: Venta[] = [];
+  datosListaVenta = new MatTableDataSource(this.datoInicio);
+  @ViewChild(MatPaginator) paginacionTabla!: MatPaginator;
+
+
+  constructor(
+    private fb: FormBuilder,
+    private _ventaService: VentaService,
+    private _utilidadService: UtilidadService,
+    private dialog: MatDialog
+  ) {
+    this.frmBusqueda = this.fb.group({
+      buscarPor: ["fecha"],
+      numero: [""],
+      fechaInicio: [""],
+      fechaFin: [""]
+    });
+
+    this.frmBusqueda.get('buscarPor')?.valueChanges.subscribe((value) => {
+      
+        this.frmBusqueda.patchValue({
+          numero: "",
+          fechaInicio: "",
+          fechaFin: ""
+        });
+    });
   }
 
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+    this.datosListaVenta.paginator = this.paginacionTabla;
+  }
+
+  aplicarFiltroTabla(event: Event) {
+    const filterValue = (event?.target as HTMLInputElement).value;
+    this.datosListaVenta.filter = filterValue.trim().toLowerCase();
+  }
+
+  buscarVentas() {
+    let _fechaInicio: string = '';
+    let _fechaFin: string = '';
+
+    if(this.frmBusqueda.value.buscarPor === 'fecha') {
+      if (this.frmBusqueda.value.fechaInicio) {
+        _fechaInicio = moment(this.frmBusqueda.value.fechaInicio).format('DD/MM/YYYY');
+        _fechaFin = moment(this.frmBusqueda.value.fechaFin).format('DD/MM/YYYY');
+      }
+      if (_fechaInicio === "Invalid Date" || _fechaFin === "Invalid Date") {
+        this._utilidadService.mostrarAlerta('Debe ingresar ambas fechas.', 'Error');
+        return
+      }
+    }
+    this._ventaService.historial(
+      this.frmBusqueda.value.buscarPor,
+      this.frmBusqueda.value.numero,
+      _fechaInicio,
+      _fechaFin
+    ).subscribe({
+      next: (data) => {
+        if (data.status) 
+          this.datosListaVenta = data.value;
+        else 
+          this._utilidadService.mostrarAlerta('No se encontraron resultados.', 'Información');        
+      },
+      error: (error) => {
+        console.error(error);
+        this._utilidadService.mostrarAlerta('Error al buscar ventas.', 'Error');
+      }
+    });    
+  }
+
+  verDetalleVenta(_venta: Venta) {
+    const dialogRef = this.dialog.open(ModalDetalleVentaComponent, {
+      width: '700px',
+      data: _venta,
+      disableClose: true,
+    });
+  }
 }
--- src/app/Components/layout/layout.module.ts
+++ src/app/Components/layout/layout.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+
 import { LayoutRoutingModule } from './layout-routing.module';
 import { DashboardComponent } from './Pages/dashboard/dashboard.component';
 import { UsuarioComponent } from './Pages/usuario/usuario.component';
@@ -11,6 +14,7 @@ import { ReporteComponent } from './Pages/reporte/reporte.component';
 import { SharedModule } from 'src/app/Reutilizables/shared/shared.module';
 import { ModalUsuarioComponent } from './Modals/modal-usuario/modal-usuario.component';
 import { ModalProductoComponent } from './Modals/modal-producto/modal-producto.component';
+import { ModalDetalleVentaComponent } from './Modals/modal-detalle-venta/modal-detalle-venta.component';
 
 
 @NgModule({
@@ -22,9 +26,12 @@ import { ModalProductoComponent } from './Modals/modal-producto/modal-producto.c
     HistorialVentaComponent,
     ReporteComponent,
     ModalUsuarioComponent,
-    ModalProductoComponent
+    ModalProductoComponent,
+    ModalDetalleVentaComponent
   ],
   imports: [
+    MatDatepickerModule,
+    MatMomentDateModule,
     CommonModule,
     LayoutRoutingModule,
     SharedModule
